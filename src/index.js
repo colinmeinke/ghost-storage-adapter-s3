092f3c55e725d531b644d2c0c08dc4c2a759801e
Colin Meinke <hello@colinmeinke.com>
2016-10-09 23:12:20 +0200

    chore(lint): add standard code style

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import AWS from 'aws-sdk';
-import BaseStore from '../../../core/server/storage/base';
-import { join } from 'path';
-import Promise, { promisify } from 'bluebird';
-import { readFile } from 'fs';
+import AWS from 'aws-sdk'
+import BaseStore from '../../../core/server/storage/base'
+import { join } from 'path'
+import Promise, { promisify } from 'bluebird'
+import { readFile } from 'fs'
 
-const readFileAsync = promisify( readFile );
+const readFileAsync = promisify(readFile)
 
-const stripLeadingSlash = s => s.indexOf( '/' ) === 0 ? s.substring( 1 ) : s;
+const stripLeadingSlash = s => s.indexOf('/') === 0 ? s.substring(1) : s
 
 class Store extends BaseStore {
-  constructor ( config = {}) {
-    super( config );
+  constructor (config = {}) {
+    super(config)
 
-    AWS.config.setPromisesDependency( Promise );
+    AWS.config.setPromisesDependency(Promise)
 
     const {
       accessKeyId,
@@ -20,43 +20,43 @@ class Store extends BaseStore {
       bucket,
       pathPrefix,
       region,
-      secretAccessKey,
-    } = config;
+      secretAccessKey
+    } = config
 
-    this.accessKeyId = accessKeyId;
-    this.bucket = bucket;
-    this.host = assetHost ? assetHost : `https://s3${ region === 'us-east-1' ? '' : `-${ region }` }.amazonaws.com/${ bucket }`;
-    this.pathPrefix = stripLeadingSlash( pathPrefix || '' );
-    this.region = region;
-    this.secretAccessKey = secretAccessKey;
+    this.accessKeyId = accessKeyId
+    this.bucket = bucket
+    this.host = assetHost || `https://s3${region === 'us-east-1' ? '' : `-${region}`}.amazonaws.com/${bucket}`
+    this.pathPrefix = stripLeadingSlash(pathPrefix || '')
+    this.region = region
+    this.secretAccessKey = secretAccessKey
   }
 
-  delete ( fileName, targetDir ) {
-    const directory = targetDir || this.getTargetDir( this.pathPrefix );
+  delete (fileName, targetDir) {
+    const directory = targetDir || this.getTargetDir(this.pathPrefix)
 
-    return new Promise(( resolve, reject ) => {
+    return new Promise((resolve, reject) => {
       return this.s3()
         .deleteObject({
           Bucket: this.bucket,
-          Key: stripLeadingSlash( join( directory, fileName )),
+          Key: stripLeadingSlash(join(directory, fileName))
         })
         .promise()
-        .then(() => resolve( true ))
-        .catch(() => resolve( false ));
-    });
+        .then(() => resolve(true))
+        .catch(() => resolve(false))
+    })
   }
 
-  exists ( fileName ) {
-    return new Promise(( resolve, reject ) => {
+  exists (fileName) {
+    return new Promise((resolve, reject) => {
       return this.s3()
         .getObject({
           Bucket: this.bucket,
-          Key: stripLeadingSlash( fileName ),
+          Key: stripLeadingSlash(fileName)
         })
         .promise()
-        .then(() => resolve( true ))
-        .catch(() => resolve( false ));
-    });
+        .then(() => resolve(true))
+        .catch(() => resolve(false))
+    })
   }
 
   s3 () {
@@ -64,38 +64,38 @@ class Store extends BaseStore {
       accessKeyId: this.accessKeyId,
       bucket: this.bucket,
       region: this.region,
-      secretAccessKey: this.secretAccessKey,
-    });
+      secretAccessKey: this.secretAccessKey
+    })
   }
 
-  save ( image, targetDir ) {
-    const directory = targetDir || this.getTargetDir( this.pathPrefix );
+  save (image, targetDir) {
+    const directory = targetDir || this.getTargetDir(this.pathPrefix)
 
-    return new Promise(( resolve, reject ) => {
+    return new Promise((resolve, reject) => {
       Promise.all([
-        this.getUniqueFileName( this, image, directory ),
-        readFileAsync( image.path ),
+        this.getUniqueFileName(this, image, directory),
+        readFileAsync(image.path)
       ]).then(([ fileName, file ]) => (
         this.s3()
           .putObject({
             ACL: 'public-read',
             Body: file,
             Bucket: this.bucket,
-            CacheControl: `max-age=${ 30 * 24 * 60 * 60 }`,
+            CacheControl: `max-age=${30 * 24 * 60 * 60}`,
             ContentType: image.type,
-            Key: stripLeadingSlash( fileName ),
+            Key: stripLeadingSlash(fileName)
           })
           .promise()
-          .then(() => resolve( `${ this.host }/${ fileName }` ))
-      )).catch( error => reject( error ));
-    });
+          .then(() => resolve(`${this.host}/${fileName}`))
+      )).catch(error => reject(error))
+    })
   }
 
   serve () {
-    return ( req, res, next ) => {
-      next();
-    };
+    return (req, res, next) => {
+      next()
+    }
   }
 }
 
-export default Store;
+export default Store
